bd8bb0027ce101e3b160911b12fe7c17cba365ad
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2025-10-14 14:15:24 -0600

    Agregamos el código para inicializar las extensiones.

--- app-vacantes/app/__init__.py
+++ app-vacantes/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes.usuarios_routes import usuarios_bp
+from app.extensions import db
 # Crear e iniciar nuestra aplicación, devolver una instancia de la clase Flask cn las configuraciones asignadas.
 
 # Definimos la función
@@ -7,6 +8,9 @@ def create_app():
     # Instancear la clase
     app = Flask(__name__)
     
+    # Inicializar las extensiones
+    db.init_app(app)
+
     # Cargar una configuracion
     app.config.from_object('config.Config')
     
