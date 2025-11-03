5f1f711d0ced05ca6589cd61aaa54edac8306ca7
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2025-11-03 17:54:06 -0600

    Se agregó la función para retornar un diccionario. También se importó DateTime para las fechas.

--- app-vacantes/app/models/VacantesModel.py
+++ app-vacantes/app/models/VacantesModel.py
@@ -1,5 +1,6 @@
 # En este archivo iran las tablas o modelos que se van a crear en la base de datos.
 from app.extensions import db  # Importar la instancia de db
+from datetime import datetime
 
 class VacantesModel(db.Model):
     __tablename__ = 'vacantes'  # Nombre de la tabla en la base de datos.
@@ -8,10 +9,24 @@ class VacantesModel(db.Model):
     nombre_vacante = db.Column (db.String(50), nullable = False, unique = True)
     descripcion = db.Column (db.Text)
     detalles = db.Column(db.Text)
-    fecha_publicacion = db.Columb(db.Date)
-    fecha_edicion = db.Column(db.Date)
+    fecha_publicacion = db.Column(db.DateTime, default=datetime.utcnow)
+    fecha_edicion = db.Column(db.DateTime, default=datetime.utcnow)
     estado = db.Column(db.String(20), default = "Disponible")
     creador = db.Column(db.String(50))
     postulador = db.Column(db.String(50))
     
+    # def to_dict sirve para retornar un diccionario
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'nombre_vacante': self.nombre_vacante,
+            'descripcion': self.descripcion,
+            'detalles': self.detalles,
+            # En esta parte se serializa a ISO8601 para que JSON muestre fecha y hora correctamente.
+            'fecha_publicacion': self.fecha_publicacion.isoformat() if self.fecha_publicacion else None,
+            'fecha_edicion': self.fecha_edicion.isoformat() if self.fecha_edicion else None,
+            'estado': self.estado,
+            'creador': self.creador,
+            'postulador': self.postulador
+        }
     
