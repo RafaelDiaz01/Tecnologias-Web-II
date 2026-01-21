96b341b01ce7ddfff9fc0f417a2d0a558e7d83d8
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-20 22:26:43 -0600

    Cambiar texto del footer

--- app-vacantes/app-react/src/components/Footer.jsx
+++ app-vacantes/app-react/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="text-center text-sm text-slate-400 py-6">
-            © 2026 My Company, Inc.
+            © 2026 Universidad de la Sierra Juárez
         </footer>
     );
 };
