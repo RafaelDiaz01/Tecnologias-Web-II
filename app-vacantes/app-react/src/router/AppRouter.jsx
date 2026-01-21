04919a124f2129ed63a6c59442ea68b044576d7c
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-21 15:21:15 -0600

    Agregar ruta de la page Mensaje y la page Index

--- app-vacantes/app-react/src/router/AppRouter.jsx
+++ app-vacantes/app-react/src/router/AppRouter.jsx
@@ -4,16 +4,20 @@ import Admin from "../pages/Admin.jsx";
 import CrearVacante from "../pages/CrearVacante.jsx";
 import Vacantes from "../pages/Vacantes.jsx";
 import Detalle from "../pages/Detalle.jsx";
+import Mensaje from "../pages/Mensaje.jsx";
+import Index from "../pages/Index.jsx";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="*" element={<Mensaje />} />
       <Route path="/login" element={<Login />} />
       <Route path="/admin" element={<Admin />} />
       <Route path="/crear-vacante" element={<CrearVacante />} />
       <Route path="/vacantes" element={<Vacantes />} />
       <Route path="/detalle/:id" element={<Detalle />} />
+      <Route path="/index" element={<Index />} />
     </Routes>
   );
 };
