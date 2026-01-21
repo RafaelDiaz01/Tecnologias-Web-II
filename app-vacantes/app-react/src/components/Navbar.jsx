0c2be721f3aa23d6d645071c1bbb3909a44722e5
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-21 15:17:20 -0600

    Agregar ruta de la page Index al gacer click en el nombre de la App

--- app-vacantes/app-react/src/components/Navbar.jsx
+++ app-vacantes/app-react/src/components/Navbar.jsx
@@ -1,39 +1,43 @@
 import { NavLink } from "react-router-dom";
-import {
-    LogOut,
-    PlusCircle,
-    Briefcase,
-} from "lucide-react";
+import { LogOut, PlusCircle, Briefcase } from "lucide-react";
 
 const linkClass = ({ isActive }) =>
-    `flex items-center gap-2 px-4 py-2 rounded-lg text-sm font-medium transition ${isActive ? "text-sm font-semibold" : "text-slate-600 hover:bg-[var(--color-primario)] hover:text-[var(--color-texto)]"
-    }`;
+  `flex items-center gap-2 px-4 py-2 rounded-lg text-sm font-medium transition ${
+    isActive
+      ? "text-sm font-semibold"
+      : "text-slate-600 hover:bg-[var(--color-primario)] hover:text-[var(--color-texto)]"
+  }`;
 
 export default function Navbar() {
-    return (
-        <header className="w-full bg-[var(--color-fondo)] px-8 py-6">
-            <div className="mx-auto px-4 py-4 flex flex-col sm:flex-row items-center justify-between gap-4">
-                <h1 className="text-lg font-semibold text-slate-700">
-                    Vacantes <span className="text-[var(--color-secundario)]">App</span>
-                </h1>
+  return (
+    <header className="w-full bg-[var(--color-fondo)] px-8 py-6">
+      <div className="mx-auto px-4 py-4 flex flex-col sm:flex-row items-center justify-between gap-4">
+        <h1 className="text-lg font-semibold text-slate-700">
+          <NavLink to="/index">
+            Vacantes <span className="text-[var(--color-secundario)]">App</span>
+          </NavLink>
+        </h1>
 
-                <nav className="flex gap-3">
-                    <NavLink to="/crear-vacante" className={linkClass}>
-                        <PlusCircle size={18} />
-                        Crear Vacante
-                    </NavLink>
+        <nav className="flex gap-3">
+          <NavLink to="/crear-vacante" className={linkClass}>
+            <PlusCircle size={18} />
+            Crear Vacante
+          </NavLink>
 
-                    <NavLink to="/vacantes" className={linkClass}>
-                        <Briefcase size={18} />
-                        Vacantes
-                    </NavLink>
+          <NavLink to="/vacantes" className={linkClass}>
+            <Briefcase size={18} />
+            Vacantes
+          </NavLink>
 
-                    <NavLink to="/login" className="flex items-center gap-2 px-4 py-2 rounded-lg text-sm font-medium text-red-600 transition text-[var(--color-cancel)] hover:bg-[var(--color-cancel)] hover:text-[var(--color-texto)]">
-                        <LogOut size={18} />
-                        Salir
-                    </NavLink>
-                </nav>
-            </div>
-        </header>
-    )
+          <NavLink
+            to="/login"
+            className="flex items-center gap-2 px-4 py-2 rounded-lg text-sm font-medium text-red-600 transition text-[var(--color-cancel)] hover:bg-[var(--color-cancel)] hover:text-[var(--color-texto)]"
+          >
+            <LogOut size={18} />
+            Salir
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 }
