f50841ca73765811606378f53e21f038c2c4ca80
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-21 00:05:42 -0600

    Crear page Vacantes usando MUI Datagrid para manejar la tabla de vacantes

---
+++ app-vacantes/app-react/src/pages/Vacantes.jsx
@@ -0,0 +1,50 @@
+import PageLayout from "../components/layouts/PageLayout";
+import { DataGrid, GridActionsCellItem, GridActionsCell } from '@mui/x-data-grid';
+import { Eye, Trash } from "lucide-react";
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'nombre', headerName: 'Vacante', flex: 1 },
+    { field: 'publicado', headerName: 'Publicado', width: 150 },
+    {
+        field: 'acciones', type: 'actions', headerName: 'Acciones', width: 150, renderCell: (params) => (
+            <GridActionsCell {...params}>
+                <GridActionsCellItem
+                    icon={<Eye />}
+                    label="Ver Detalles"
+                />
+                <GridActionsCellItem
+                    icon={<Trash />}
+                    label="Eliminar"
+                />
+            </GridActionsCell>
+        ),
+    }
+];
+
+const rows = [
+    { id: 1, nombre: 'Programador Junior', publicado: '12-01-2026' },
+    { id: 2, nombre: 'Chef', publicado: '12-01-2026' },
+    { id: 3, nombre: 'Diseñador Gráfico', publicado: '12-01-2026' },
+];
+
+export default function Vacantes() {
+    return (
+        <PageLayout>
+            <div className="w-full flex flex-col items-center justify-center">
+                <div className="max-w-3xl mx-auto" style={{ width: '100%' }}>
+                    <DataGrid
+                        rows={rows}
+                        columns={columns}
+                        pageSizeOptions={[5, 10]}
+                        initialState={{
+                            pagination: {
+                                paginationModel: { page: 0, pageSize: 5 },
+                            },
+                        }}
+                    />
+                </div>
+            </div>
+        </PageLayout>
+    );
+};
