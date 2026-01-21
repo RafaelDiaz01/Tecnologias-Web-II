6fb703fc383b5b700db45801f9253a13d199c96a
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-21 00:21:20 -0600

    Agregar ThemeProvider y envolver la App

--- app-vacantes/app-react/src/main.jsx
+++ app-vacantes/app-react/src/main.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import theme from './theme/theme';
 import './assets/styles/global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
