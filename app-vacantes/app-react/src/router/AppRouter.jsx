485ab2219314bfcc4f94d10292d4fa53e02044ba
RafaelDiaz01 <rafaeldiazmx98@gmail.com>
2026-01-21 10:01:48 -0600

    Agregar ruta de la page Detalle

--- app-vacantes/app-react/src/router/AppRouter.jsx
+++ app-vacantes/app-react/src/router/AppRouter.jsx
@@ -3,6 +3,7 @@ import Login from "../pages/Login.jsx";
 import Admin from "../pages/Admin.jsx";
 import CrearVacante from "../pages/CrearVacante.jsx";
 import Vacantes from "../pages/Vacantes.jsx";
+import Detalle from "../pages/Detalle.jsx";
 
 const AppRouter = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter = () => {
       <Route path="/admin" element={<Admin />} />
       <Route path="/crear-vacante" element={<CrearVacante />} />
       <Route path="/vacantes" element={<Vacantes />} />
+      <Route path="/detalle/:id" element={<Detalle />} />
     </Routes>
   );
 };
