886d947dcc9c4b9d3765045feffba49d490d47a2
Rafael Díaz desde Laptop <rafaeldiazmx98@gmail.com>
2026-01-20 21:26:49 -0600

    Crear componente Footer

---
+++ app-vacantes/app-react/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center text-sm text-slate-400 py-6">
+            © 2026 My Company, Inc.
+        </footer>
+    );
+};
